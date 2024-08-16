9ca60c34b1768e3598fd2b09fbcbe630b6b6b38a
sasa12366 <54961843+sasa12366@users.noreply.github.com>
2024-08-16 14:14:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,9 +29,9 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
        },
         6: {
-    name: 'Twerk Race 3D',
-    appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-    promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'
+			name: 'Twerk Race 3D',
+			appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+			promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'
         }
     };
 
